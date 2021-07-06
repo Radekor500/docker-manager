43fcc66256e9ee8b1a943412135b7f6700f9cf2f
Radek <radek00500@gmail.com>
2021-07-06 20:21:57 +0200

    4

--- docker-manager/src/App.js
+++ docker-manager/src/App.js
@@ -1,10 +1,12 @@
 import Containers from "./components/containers/containers";
 import Logo from "./components/logo/logo";
+import Scroll from "./components/Scroll/Scroll"
 import { useEffect, useState } from 'react'
 
 function App() {
   const [containers, setContainers] = useState([]);
-  const [search, setSearch] = useState('');
+  // const [search, setSearch] = useState('');
+  const [ops, setOps] = useState(new Map());
 
   useEffect(() => {
     fetch('http://192.168.0.134:5555/containers/json?all=true', {
@@ -19,11 +21,58 @@ function App() {
 
 })
 
+let handleOpt = (event) => {
+  let msg = document.querySelector('.alert-wrap');
+  for (const container of ops) {
+      if (container[1]) {
+          let name = container[0].slice(1)
+          console.log(name);
+          fetch(`http://192.168.0.134:5555/containers/${name}/${event.target.value}`, {
+              method: 'post',
+              headers: {'content-type': 'application/json'}
+          })
+          .then(resp => {
+              let alert = document.createElement('div');
+              if (resp.status === 204) {
+                  if (event.target.value === 'stop') {
+                      alert.style.backgroundColor = '#f44336'
+                  } else {
+                      alert.style.backgroundColor = '#04AA6D'
+                  }
+                  alert.innerHTML = name;
+              } else {
+                  alert.style.backgroundColor = '#c2790b'
+                  alert.innerHTML = 'Connection error'
+              }
+              alert.classList.add('alert')
+              msg.appendChild(alert)
+          
+              setTimeout(() => {
+                  alert.remove();
+              },10000)
+
+          })
+      }
+      
+      
+  }
+  let checkboxes = document.querySelectorAll('.check');
+  checkboxes.forEach(elem => elem.checked = false)
+}
+
+let onCheck = (event) => {
+  setOps((prevOps) => 
+      prevOps.set(event.target.value, event.target.checked)
+  )
+}
+
 
   return (
     <div>
       <Logo></Logo>
-      <Containers containers={containers}></Containers>
+      <Containers onCheck={onCheck} handleOpt={handleOpt} containers={containers}></Containers>
+
+      
     </div>
   );
 }
--- docker-manager/src/components/containers/containers.js
+++ docker-manager/src/components/containers/containers.js
@@ -1,85 +1,17 @@
-import { useEffect, useState } from 'react'
+import {useState } from 'react'
 import './containers.css'
-function Containers({containers}) {
+function Containers({containers, handleOpt, onCheck}) {
     //const [containers, setContainers] = useState([]);
     const [search, setSearch] = useState('');
-    const [ops, setOps] = useState(new Map());
-    // useEffect(() => {
-    //     fetch('http://192.168.0.134:5555/containers/json', {
-    //         method: 'get',
-    //         headers: {'content-type': 'application/json'}
-    //     })
-    //     .then(resp => resp.json())
-    //     .then(data => {
-    //         setContainers([...data])
-    //     })
-    //     //console.log(containers)
 
-    // })
 
     let onSearch = (event) => {
         setSearch(event.target.value);
-       
-        console.log(containers)
-        //console.log(search)
     }
 
-    let onCheck = (event) => {
-        //setOps([...event.id])
-        //let checkBox = document.querySelectorAll('.check');
-        setOps((prevOps) => 
-            prevOps.set(event.target.value, event.target.checked)
-        )
-        console.log(ops)
-    }
+    
 
-    let handleOpt = (event) => {
-        console.log(event.target.value)
-        let msg = document.querySelector('.grid-wrap');
-       
-        for (const container of ops) {
-            if (container[1]) {
-                let name = container[0].slice(1)
-                console.log(name);
-                fetch(`http://192.168.0.134:5555/containers/${name}/${event.target.value}`, {
-                    method: 'post',
-                    headers: {'content-type': 'application/json'}
-                })
-                .then(resp => {
-                    if (resp.status === 204) {
-                       
-                        //alert.classList.add('alert')
-                        // msg.style.display = 'block'
-                        let alert = document.createElement('div');
-                        alert.classList.add('alert')
-                        if (event.target.value === 'stop') {
-                            //alert.classList.add('alert', 'stop')
-                            alert.style.backgroundColor = '#f44336'
-                            
-                        } else {
-                            //alert.classList.add('alert', 'success')
-                            alert.style.backgroundColor = '#04AA6D'
-                        }
-                        msg.appendChild(alert)
-                        alert.innerHTML = name;
-                        //msg.appendChild(<strong>{name}</strong>);
-                    }
 
-                })
-                // .then(data => {
-                //     // data ? console.log(data) : console.log('stopped');
-                //     console.log(data)
-                // })
-            }
-            
-            
-        }
-        let checkboxes = document.querySelectorAll('.check');
-        checkboxes.forEach(elem => elem.checked = false)
-        // ops.forEach(elem => {
-        //     console.log(elem)
-        // })
-    }
 
     const filtered = containers.filter(elem => {
         return elem.Names[0].toLowerCase().includes(search.toLowerCase())
@@ -89,7 +21,6 @@ function Containers({containers}) {
         return (
             filtered.map((container, index) => {
                 return (
-                    //console.log(container)
                     <tr key={index}>
                         <td><input onChange={onCheck} className='check' value={container.Names} type='checkbox'></input> {container.Names}</td>
                         <td>{container.State}</td>
@@ -113,7 +44,8 @@ function Containers({containers}) {
                     <button>Restart</button>
                     <button>Remove</button>
                     <button value='stop' onClick={handleOpt}>Stop</button>
-                </div>  
+                </div>
+                <div className='alert-wrap'></div>  
                 <table>
                     <tr>
                         <th>Name</th>
